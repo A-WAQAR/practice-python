ba52f1a5ea756e7f9cc503a27c9afa5f805f8fd3
A-WAQAR <aasifwaqar933@gmail.com>
2026-05-05 18:07:42 +0530

    weather_app.py
    Added weather code

--- weather_app/weather_app.py
+++ weather_app/weather_app.py
@@ -1,31 +1,31 @@
 
-import requests
+# import requests
 
-def show_weather():
-    API_KEY = "f35231d22a4478c49fdf18398b134507"
-    CITY = input("enter city name: ")
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
-    try: 
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            temperature = data["main"]["temp"]
-            weather_desc = data['weather'][0]['description']
-            humidity = data['main']['humidity']
-            print(f" -----the weather of : {CITY} ----")
-            print(f"temperature: {temperature}°C")
-            print(f"weather_desc : {weather_desc.capitalize()}")
-            print(f"humidity: {humidity}%")
-        else:
-            print(f"Error, check it please: {response.status_code}")
-            print("please check your API_KEY nad CITY name")
+# def show_weather():
+#     API_KEY = "f35231d22a4478c49fdf18398b134507"
+#     CITY = input("enter city name: ")
+#     url = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+#     try: 
+#         response = requests.get(url)
+#         if response.status_code == 200:
+#             data = response.json()
+#             temperature = data["main"]["temp"]
+#             weather_desc = data['weather'][0]['description']
+#             humidity = data['main']['humidity']
+#             print(f" -----the weather of : {CITY} ----")
+#             print(f"temperature: {temperature}°C")
+#             print(f"weather_desc : {weather_desc.capitalize()}")
+#             print(f"humidity: {humidity}%")
+#         else:
+#             print(f"Error, check it please: {response.status_code}")
+#             print("please check your API_KEY nad CITY name")
 
-    except Exception as e:
-        print("error:", e)
-while True:
-    print("\n---")
-    print("1. show weather")
+#     except Exception as e:
+#         print("error:", e)
+# while True:
+#     print("\n---")
+#     print("1. show weather")
 
-    choice = input("enter choice: ")
-    if choice == "1":
-        show_weather()
+#     choice = input("enter choice: ")
+#     if choice == "1":
+#         show_weather()
